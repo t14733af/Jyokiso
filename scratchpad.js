897f42c280c8626a3ca263bef5a2453a7b4c15a5
t14733af <fukufuku0319@net73-dhcp19.sfc.keio.ac.jp>
2014-10-09 10:13:16 +0900

    /Users/fukufuku0319/Jyokiso/scratchpad.js
    kadai2

---
+++ scratchpad.js
@@ -0,0 +1,30 @@
+
+var add = function(a,b){
+  var result;
+  result = a + b;
+  return result;
+};
+add(10,5);
+
+
+
+
+var kakezan = function(a,b){
+  return a * b;
+};
+kakezan(10,5);
+
+var f = function(x){
+  var result, tmp;
+  tmp = multiply(2,x);
+  result = add(tmp,10);
+  return result;
+};
+f(10);
+/*
+
+
+
+
+
+
